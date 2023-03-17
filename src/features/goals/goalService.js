ca962b750a340da0138e37e447898a490b1d6efc
Haripriya Chittari <hpvarma09@gmail.com>
2023-03-17 14:39:31 +0530

    Update goalService.js

--- src/features/goals/goalService.js
+++ src/features/goals/goalService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL='/api/goals/'
+const API_URL='https://goalsetter-backend.onrender.com/api/goals/'
 
 const createGoal=async(goalData,token)=>{
     const config={
